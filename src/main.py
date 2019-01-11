bfd6025e8aaa38261b72d79fc12c0bb9c9131f34
lusy <vaseva@mi.fu-berlin.de>
2019-01-11 10:05:26 +0100

    Generate more detailed list of filters

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def sort_actions(filepath):
 
 def get_filters_actions(in_file, out_file):
     df = pd.read_csv(in_file, sep=',')
-    df_update = df[['af_id', 'af_hidden', 'af_global', 'af_actions', 'af_hit_count', 'af_public_comments']]
+    df_update = df[['af_id', 'af_hidden', 'af_global', 'af_enabled', 'af_deleted', 'af_throttled', 'af_group', 'af_timestamp', 'af_actions', 'af_hit_count', 'af_public_comments']]
     df_update.to_csv(out_file, sep='\t')
     #print(df[['af_id', 'af_hidden', 'af_actions', 'af_hit_count', 'af_public_comments']])
 
@@ -26,7 +26,7 @@ main
 '''
 def main():
     #sort_actions('../filter-lists/20181215-en-enabled-and-disabled')
-    get_filters_actions('../quarries/quarry-32518-all-filters-sorted-num-hits.csv', '../filter-lists/20190106115600_filters-sorted-by-hits-description')
+    get_filters_actions('../quarries/quarry-32518-all-filters-sorted-num-hits.csv', '../filter-lists/20190106115600_filters-sorted-by-hits')
 
 if __name__ == '__main__':
     status = main()
