b31910fa2ebd00ebe33cf9a053ffc4390de6c9fb
lusy <vaseva@mi.fu-berlin.de>
2019-02-14 16:37:49 +0100

    Add random notes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from mw import database
 
 
 def read_filters(filepath):
@@ -20,6 +21,25 @@ def get_filters_actions(in_file, out_file):
     df_update = df[['af_id', 'af_hidden', 'af_global', 'af_enabled', 'af_deleted', 'af_throttled', 'af_group', 'af_timestamp', 'af_actions', 'af_hit_count', 'af_public_comments']]
     df_update.to_csv(out_file, sep='\t')
     #print(df[['af_id', 'af_hidden', 'af_actions', 'af_hit_count', 'af_public_comments']])
+
+
+def download_db_table():
+    db = database.DB.from_params(
+        host="analytics-store.eqiad.wmnet",
+        read_default_file="~/.my.cnf",
+        user="research",
+        db="enwiki"
+    )
+
+    users = db.users.query(
+        registered_after="20140101000000",
+        direction="newer",
+        limit=10
+    )
+
+for user in users:
+print("{user_id}:{user_name} -- {user_editcount} edits".format(**user))
+
 
 '''
 main
